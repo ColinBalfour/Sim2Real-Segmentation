946f68890891df03fad758cef235309618ab1e7a
felixng <ngthinh302@gmail.com>
2024-09-04 17:03:44 -0400

    minor improvements to the init code

--- part2/loadParam.py
+++ part2/loadParam.py
@@ -1,15 +1,15 @@
 import os
 
-HOME_PATH   =   os.path.expanduser("~")
-JOB_ID      =   "run2"
-MODEL_NAME  =   "windowseg"
-DS_PATH     =   "/home/manoj/blender/flyingWindows/og/"
-OUT_PATH    =   "/home/manoj/outputs/windowseg/"
+HOME_PATH = os.path.expanduser("~")
+JOB_ID = "run2"
+MODEL_NAME = "windowseg"
+DS_PATH = os.path.join(HOME_PATH, "blender/flyingWindows/og/")
+OUT_PATH = os.path.join(HOME_PATH, "outputs/windowseg/")
 
-JOB_FOLDER  =   os.path.join(OUT_PATH, JOB_ID)
-TRAINED_MDL_PATH    =   os.path.join(JOB_FOLDER, "parameters")
-BATCH_SIZE          =   8
-LR                  =   1e-4
-LOG_BATCH_INTERVAL  =   1
+JOB_FOLDER = os.path.join(OUT_PATH, JOB_ID)
+TRAINED_MDL_PATH = os.path.join(JOB_FOLDER, "parameters")
+BATCH_SIZE = 8
+LR = 1e-4
+LOG_BATCH_INTERVAL = 1
 LOG_WANDB = True
-NUM_WORKERS  =   1
+NUM_WORKERS = 1
